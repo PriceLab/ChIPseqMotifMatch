a842f7f6b4be23359fa8fbbb090a1c29e7ae7cf9
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-09-23 06:17:39 -0700

    small demo of MACS2 and fimo

---
+++ bulk/chr19-demo/addFimoToMACS2NarrowPeaks.R
@@ -0,0 +1,101 @@
+library(MotifDb)
+source(file.path(Sys.getenv("HOME"), "github", "fimoService", "batchMode", "fimoBatchTools.R"))
+library(igvR)
+#------------------------------------------------------------------------------------------------------------------------
+# a convenience function
+printf <- function(...) print(noquote(sprintf(...)))
+#------------------------------------------------------------------------------------------------------------------------
+if(!exists("tbl.narrowPeaks")){
+   tbl.narrowPeaks <- read.table("demo_peaks.narrowPeak", sep="\t", as.is=TRUE)
+   colnames(tbl.narrowPeaks) <- c("chrom", "start", "end", "name", "score", "strand", "foldChange",
+                                  "pScore", "qScore", "summitPos")
+   }
+
+if(!file.exists("ctcf-human.meme")){
+   motif <- query(MotifDb, c("ctcf", "sapiens", "jaspar2018", "MA0139"))
+   export(motif, con="ctcf-human.meme", format="meme")
+   }
+#------------------------------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+   test_addFimoToNarrowPeaks()
+
+} # runTests
+#------------------------------------------------------------------------------------------------------------------------
+# 1e-4 is the traditional FIMO pValue threshold: only motif-to-sequence matches better than this
+# are returned.  we find that looseing this to at least 1e-3 is needed to find motifs in high-scoring
+# MACS2-called narrow peaks.
+addFimoToNarrowPeaks <- function(chromosome, fimo.threshold, numberOfPeaksToConsider=-1, plot=FALSE)
+{
+   tbl.np <- subset(tbl.narrowPeaks, chrom==chromosome)
+
+   if(numberOfPeaksToConsider > 0)   # this makes for quick testing
+      tbl.np <- tbl.np[seq_len(numberOfPeaksToConsider),]
+
+   printf("narrowPeak rows: %d", nrow(tbl.np))
+   print(system.time(tbl.match <- fimoBatch(tbl.np, matchThreshold=fimo.threshold, genomeName="hg19",
+                                            pwmFile="ctcf-human.meme")))
+   printf(" matches from fimo: %d", nrow(tbl.match))
+
+   tbl.ov <- as.data.frame(findOverlaps(GRanges(tbl.match), GRanges(tbl.np), type="within"))
+   printf("overlaps: %d", nrow(tbl.ov))
+   colnames(tbl.ov) <- c("fimo", "narrowPeak")
+
+   tbl.noMatch <- data.frame(motif.chrom=NA, motif.start=NA_integer_, motif.end=NA_integer_,
+                             motif.tf=NA, motif.strand=NA, motif.score=NA_real_, motif.p.value=1,
+                             motif.matched_sequence=NA, motif.motif_id=NA)
+
+
+     # a motif often matches at multiple points within a sequence.  we want just the best match
+     # this function is used in the lapply which follows
+
+   bestFimoRow <- function(narrowPeakRow){
+      fimoRows=subset(tbl.ov, narrowPeak==narrowPeakRow)$fimo;
+      tbl.matchThisPeak <- tbl.match[fimoRows,];
+      tbl.bestMatch <- subset(tbl.matchThisPeak, score==max(tbl.matchThisPeak$score))[1,]
+      grep(tbl.bestMatch$start, tbl.match$start)[1]
+      }
+
+   x <- lapply(unique(tbl.ov$narrowPeak), bestFimoRow)
+   tbl.ov.trimmed <- data.frame(fimo=unlist(x), narrowPeak=unique(tbl.ov$narrowPeak))
+   unmatched.peaks <- setdiff(seq_len(nrow(tbl.np)), tbl.ov.trimmed$narrowPeak)
+   length(unmatched.peaks)
+
+   tbl.matchFiltered <- tbl.match[tbl.ov.trimmed$fimo,]
+   dim(tbl.matchFiltered)
+   colnames(tbl.matchFiltered) <- paste0("motif.", colnames(tbl.matchFiltered))
+
+   tbl.both <- cbind(tbl.np[tbl.ov.trimmed$narrowPeak,], tbl.matchFiltered, row.names=NULL)
+   dim(tbl.both)
+
+   if(length(unmatched.peaks) > 0){
+      printf("unmatched.peaks: %d", length(unmatched.peaks))
+      tbl.unmatched <- cbind(tbl.np[unmatched.peaks,], tbl.noMatch, row.names=NULL)
+      tbl.both <- rbind(tbl.both, tbl.unmatched)
+      }
+
+   dim(tbl.both)
+   new.order <- order(tbl.both$start, decreasing=FALSE)
+   tbl.both <- tbl.both[new.order,]
+
+   checkEquals(tbl.both$start, tbl.np$start)
+   if(plot)
+      with(tbl.both, plot(score, -log10(motif.p.value), main=chromosome))
+
+   tbl.both
+
+} # addFimoToNarrowPeaks
+#------------------------------------------------------------------------------------------------------------------------
+test_addFimoToNarrowPeaks <- function()
+{
+   printf("--- test_addFimoToNarrowPeaks")
+
+   tbl.10.3 <- addFimoToNarrowPeaks("chr19", 1e-3, 10)
+
+   checkEquals(dim(tbl.10.3), c(10,19))
+
+
+
+} # test_addFimoToNarrowPeaks
+#------------------------------------------------------------------------------------------------------------------------
+
