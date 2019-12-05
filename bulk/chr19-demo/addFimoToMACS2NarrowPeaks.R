98d3e708567d4274be3189ed3961d838e8163898
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2019-12-04 23:00:04 -0800

    Update addFimoToMACS2NarrowPeaks.R

--- bulk/chr19-demo/addFimoToMACS2NarrowPeaks.R
+++ bulk/chr19-demo/addFimoToMACS2NarrowPeaks.R
@@ -125,17 +125,6 @@ addConserverationScores <- function(chrom, start, end)
 } # addConservationScores
 #------------------------------------------------------------------------------------------------------------------------
 #chrom1
-if(!exists("tbl.narrowPeaks")){
-  tbl.narrowPeaks <- read.table("ctcf__peaks.narrowPeak", sep="\t", as.is=TRUE)
-  colnames(tbl.narrowPeaks) <- c("chrom", "start", "end", "name", "score", "strand", "foldChange",
-                                 "pScore", "qScore", "summitPos")
-}
-
-if(!file.exists("ctcf-human.meme")){
-  motif <- query(MotifDb, c("ctcf", "sapiens", "jaspar2018", "MA0139"))
-  export(motif, con="ctcf-human.meme", format="meme")
-}
-
 tbl.10.4 <- addFimoToNarrowPeaks(tbl.narrowPeaks, "chr1", 1e-2, 10)
 tbl.chrom1<-addConserverationScores(chrom="chr1", start=1, end=NA,tbl.10.4 )
 
